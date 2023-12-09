d188b06f7542e19d8d0110a4c8ae92efdc5acbb5
Seymone Gugneja <seymonegug@gmail.com>
2023-12-09 04:14:20 -0500

    build and register Car Model and Car Make models

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -1,13 +1,24 @@
 from django.contrib import admin
-# from .models import related models
+from .models import CarModel, CarMake
 
 
 # Register your models here.
 
 # CarModelInline class
+class CarModelInline(admin.StackedInline):
+    model = CarModel
+    extra = 2
 
 # CarModelAdmin class
+class CarModelAdmin(admin.ModelAdmin):
+    fields = ["make", "name", "dealer_id", "car_type", "year"]
 
 # CarMakeAdmin class with CarModelInline
+class CarMakeAdmin(admin.ModelAdmin):
+    inlines = [CarModelInline]
+    fields = ["name", "description"]
+
 
 # Register models here
+admin.site.register(CarModel, CarModelAdmin)
+admin.site.register(CarMake, CarMakeAdmin)
